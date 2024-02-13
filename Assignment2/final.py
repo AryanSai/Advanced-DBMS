0cc52c8bab3466cc196b90aad61441a5cd5ff62b
Aryan Sai <aryansaiarvapelly@gmail.com>
2024-02-13 10:08:36 +0530

    asmt 2

--- Assignment2/final.py
+++ Assignment2/final.py
@@ -4,7 +4,6 @@ def parse_query(query):
     parsed_query = {}
     # strip - remove leading and trailing spaces
     # group(1) - the first group of regular expression 
-    
     select_arg = re.search(r'SELECT (.+?) FROM', query)
     if select_arg:
         parsed_query['SELECT'] = select_arg.group(1).strip().split(', ')
@@ -27,39 +26,78 @@ def parse_query(query):
 
     return parsed_query
 
-def execute_select(reader,select_clause,columns):
-    if select_clause[0] == '*': # Handling SELECT * clause
+def parse_where_condition(where_clause):
+    pattern = r'(\w+)\s*(=|<|>|<=|>=)\s*(\'?)(\w+)(\'?)'
+    match = re.search(pattern, where_clause)
+    if match:
+        column_name = match.group(1)
+        comparison_operator = match.group(2)
+        comparison_value = match.group(4)
+        return column_name, comparison_operator, comparison_value
+    else:
+        return None, None, None
+ 
+def do_operation(operator, row_value, value):
+    if isinstance(row_value, int) or row_value.isdigit():
+        row_value = int(row_value)
+    if operator == '=':
+        return row_value == int(value)
+    elif operator == '>':
+        return row_value > int(value)
+    elif operator == '<':
+        return row_value < int(value)
+    elif operator == '>=':
+        return row_value >= int(value)
+    elif operator == '<=':
+        return row_value <= int(value)
+
+def execute_select(reader, parsed_query, columns):
+    select_clause = parsed_query['SELECT']
+    print("SELECT clause:", select_clause)
+    
+    where_clause = parsed_query.get('WHERE')  # get WHERE clause from parsed query
+    print("WHERE clause:", where_clause)
+    
+    if where_clause:
+        column, operator, value = parse_where_condition(where_clause)
+        print(column, value)
+        
+        # get the index of the column specified in the WHERE clause
+        column_index = columns.index(column)
+    
+    with open('output.csv', 'w') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        if select_clause[0] == '*' or select_clause[0] == 'ALL':  # handling SELECT * clause
+            csvwriter.writerow(columns)
+            csvwriter.writerows(reader)
+        else:  # handling SELECT {col1, col2, ...} clause
+            csvwriter.writerow(select_clause)
             for row in reader:
-                print(row)
-    else: # Handling SELECT {col1, col2, ...} clause
-        dict = {column: index for index, column in enumerate(columns)}
-        for row in reader:
-            print([row[dict[attr]] for attr in select_clause])
+                # apply WHERE clause if specified
+                if where_clause and do_operation(operator,row[column_index],value):
+                    selected_row = [row[columns.index(attr)] for attr in select_clause]
+                    csvwriter.writerow(selected_row)  
+    print('-----------Output written to output.csv-----------')            
 
 
 def execute(parsed_query):
     # extracting the names of the csv files
-    from_clause=parsed_query['FROM']
+    from_clause = parsed_query['FROM']
     print("FROM clause:", from_clause)
     
     # opening the csv files
     with open(from_clause) as csvfile:
-        reader = csv.reader(csvfile)
+        reader = csv.reader(csvfile) #iterator
         columns = next(reader)
         print('The columns in the table are: ',columns)
-        
-        select_clause = parsed_query['SELECT']
-        print("SELECT clause:", select_clause)
-        
-        execute_select(reader,select_clause,columns)
+        execute_select(reader,parsed_query,columns)
         
 # WHERE student.class = 'I MTech' ORDER BY student.reg_no ASC
 # name, class
 def main():
-    sql_query = "SELECT name, class FROM student.csv ;"
+    sql_query = "SELECT name FROM student.csv WHERE reg_no > 5"
     parsed_query = parse_query(sql_query)
     print("Parsed Query:", parsed_query)
-    
     execute(parsed_query)
 
 main()
--- Assignment2/trycsv.py
+++ Assignment2/trycsv.py
@@ -1,67 +1,85 @@
 import csv
 
-path = 'friends.csv'
+# path = 'student.csv'
 
-with open(path, newline='') as csvfile:
-    reader = csv.reader(csvfile)
+# with open(path, newline='') as csvfile:
+#     reader = csv.reader(csvfile)
     
-    #extract the column names
-    fields = next(reader)
-    print(fields)
+#     #extract the column names
+#     fields = next(reader)
+#     print(fields)
     
-    #print the line number----starts with 1
-    print(reader.line_num)
+#     #print the line number----starts with 1
+#     print(reader.line_num)
         
-    for row in reader: #each row is a list : The ', ' is the separator that is placed between list elements
-        print(', '.join(row))
+#     for row in reader: #each row is a list : The ', ' is the separator that is placed between list elements
+#         print(', '.join(row))
 
-rows = []      
-with open(path, newline='') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        rows.append(row)
-        print(row['name'], row['email']) #to access a specific column in each row
+# print('saoooooooooooooooooo')
+# rows = []      
+# with open(path, newline='') as csvfile:
+#     reader = csv.DictReader(csvfile)
+#     for row in reader:
+#         rows.append(row)
+#         print(row['name'], row['class']) #to access a specific column in each row
+# # print(rows)
 
+# # with open('output.csv', 'w') as csvfile:
+# #     csvwriter = csv.writer(csvfile)
+# #     csvwriter.writerows(rows)
 
-
-# print(rows)
-
-# with open('output.csv', 'w') as csvfile:
-#     csvwriter = csv.writer(csvfile)
+# # Writing to output.csv
+# with open('output.csv', 'w', newline='') as csvfile:
+#     csvwriter = csv.DictWriter(csvfile, fieldnames=fields)
+    
+#     # Write header
+#     csvwriter.writeheader()
+    
+#     # Write rows
 #     csvwriter.writerows(rows)
 
-# Writing to output.csv
-with open('output.csv', 'w', newline='') as csvfile:
-    csvwriter = csv.DictWriter(csvfile, fieldnames=fields)
-    
-    # Write header
-    csvwriter.writeheader()
-    
-    # Write rows
-    csvwriter.writerows(rows)
 
-
-# Writing to output.csv with specific fields
-with open('output2.csv', 'w', newline='') as csvfile:
-    fieldnames = ['name', 'email']  # Include only the fields you want to write
-    csvwriter = csv.DictWriter(csvfile, fieldnames=fieldnames)
+# # Writing to output.csv with specific fields
+# with open('output2.csv', 'w', newline='') as csvfile:
+#     fieldnames = ['name', 'class']  # Include only the fields you want to write
+#     csvwriter = csv.DictWriter(csvfile, fieldnames=fieldnames)
     
-    # Write header
-    csvwriter.writeheader()
+#     # Write header
+#     csvwriter.writeheader()
     
-    # Write rows with specific fields
-    for row in rows:
-        # Create a new dictionary containing only the desired fields
-        selected_fields = {fieldname: row[fieldname] for fieldname in fieldnames}
-        csvwriter.writerow(selected_fields)
+#     # Write rows with specific fields
+#     for row in rows:
+#         # Create a new dictionary containing only the desired fields
+#         selected_fields = {fieldname: row[fieldname] for fieldname in fieldnames}
+#         csvwriter.writerow(selected_fields)
         
+# print('newwwwwwwwwwwwwwwwwww')
+# column='name'
+# value='John Doe'
+# with open(path, newline='') as csvfile:
+#     reader = csv.DictReader(csvfile)
+#     for row in reader:
+#         if(row[column]==value):
+#            print(row['name'], row['class']) #to access a specific column in each row
 
 
-print('newwwwwwwwwwwwwwwwwww')
-column='name'
-value='John Doe'
-with open(path, newline='') as csvfile:
-    reader = csv.DictReader(csvfile)
+
+
+
+# Opening the CSV file
+with open('student.csv') as csvfile:
+    reader = csv.reader(csvfile)  # Iterator
+
+    # Extracting column names from the first row
+    columns = next(reader)
+    print('The columns in the table are:', columns)
+
+    # Storing the data rows
+    data = []
     for row in reader:
-        if(row[column]==value):
-           print(row['name'], row['email']) #to access a specific column in each row
+        data.append(row)
+
+# Printing the stored data
+print('The data in the CSV file:')
+for row in data:
+    print(row)
